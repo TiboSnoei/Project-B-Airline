85c24151e193de7b3709919b18e1380875244012
TiboSnoei <1117063@hr.nl>
2026-04-01 10:07:49 +0200

    making databse work

--- tools/DatabaseCreator/Program.cs
+++ tools/DatabaseCreator/Program.cs
@@ -1,6 +1,9 @@
+﻿using System;
+using System.IO;
+using Microsoft.Data.Sqlite;
 internal static class Creatdatabase
 {
-    static int Createdatabase(string[] args)
+    static int Main(string[] args)
     {
         var dbPath = Path.Combine("data", "airline.db");
         var schemaPath = Path.Combine("db", "schema.sqlite.sql");
