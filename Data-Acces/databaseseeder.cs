30b1c92efc8e65b7ad356ad665c903f176b2fde9
TiboSnoei <1117063@hr.nl>
2026-04-08 11:12:20 +0200

    Fixed DatabaseSeeder

--- Data-Acces/databaseseeder.cs
+++ Data-Acces/databaseseeder.cs
@@ -14,11 +14,11 @@ public class DatabaseSeeder
 
         string sql1 = "INSERT INTO Plane VALUES ('HR101', 250, 'Boeing-737'), ('HR102', 250, 'Boeing-737'), ('HR103', 345, 'Airbus 330'), ('HR104', 345, 'Airbus 330')";
 
-        string sql2 = "INSERT INTO Users VALUES ('1a', 'Customer', 'Bob', 'Marten', 'BobMarten_120805', 'BobMarten@gmail.com', '2026-05-01 00:00:00', 0676543566, 0)";
+        string sql2 = "INSERT INTO Users VALUES ('1', 'Customer', 'Bob', 'Marten', 'BobMarten_120805', 'BobMarten@gmail.com', '2026-05-01 00:00:00', 0676543566, 0)";
 
-        string sql3 = "INSERT INTO Flight VALUES ('1a', 'HR101', 'Berlin', 'Rotterdam', '2026-05-01 12:45:00', '2026/05/01 11:15:00', 100.00, 100.00, 100.00, 100.00, 100.00)";
+        string sql3 = "INSERT INTO Flight VALUES ('1', 'HR101', 'Berlin', 'Rotterdam', '2026-05-01 12:45:00', '2026/05/01 11:15:00', 100.00, 100.00, 100.00, 100.00, 100.00)";
 
-        string sql4 = "INSERT INTO CustomerFlight VALUES('1a', '1a', '12C', False, False, False, False)";
+        string sql4 = "INSERT INTO CustomerFlight VALUES('1', '1', '12C', False, False, False, False)";
 
         using var connection = DatabaseConnector.Open(cs);
 
@@ -27,6 +27,7 @@ public class DatabaseSeeder
         foreach (string sql in sqllist)
         {
             using SqliteCommand command = new(sql, connection);
+            command.ExecuteNonQuery();
         }
     }
 }
