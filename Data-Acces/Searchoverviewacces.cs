ab698b5066be8b76ef67772602131feda38b288f
TiboSnoei <1117063@hr.nl>
2026-04-15 15:37:52 +0200

    added missing things

--- Data-Acces/Searchoverviewacces.cs
+++ Data-Acces/Searchoverviewacces.cs
@@ -1,16 +1,60 @@
-// using System.Runtime.InteropServices;
-// using System.Security.Cryptography.X509Certificates;
-// using Microsoft.Data.Sqlite;
+using Microsoft.Data.Sqlite;
+public class SearchoverviewAcces
+{
+    private const string DatabaseLoc = "data/airline.db";
+    private readonly string cs = $"Data Source={DatabaseLoc}";
 
-// class SearchoverviewAcces
-// {
-//     public string Query;
-//     public const string DatabaseLoc = "data/airline.db";
-//     public string cs = $"Data Source={DatabaseLoc}";
-//     public SearchoverviewAcces(string query)
-//     {
-//         Query = query;
-//         using var connection = DatabaseConnector.Open(cs);
-//         cmd = new SqliteCommand(query);
-//     }
-// }
+    internal List<FlightModel> SearchFlights(SearchoverviewBuisness search)
+    {
+        var outbound = ExecuteFlightSearch(
+            destination: search.Destinationselected,
+            origin: "Rotterdam",
+            date: search.Departuredate);
+
+        if (!search.Returnflightselected)
+        {
+            return outbound;
+        }
+
+        var inbound = ExecuteFlightSearch(
+            destination: "Rotterdam",
+            origin: search.Destinationselected,
+            date: search.Returndate);
+
+        outbound.AddRange(inbound);
+        return outbound;
+    }
+
+    private List<FlightModel> ExecuteFlightSearch(string destination, string origin, DateTime date)
+    {
+        var results = new List<FlightModel>();
+
+        using var connection = DatabaseConnector.Open(cs);
+        using var cmd = connection.CreateCommand();
+
+        DateTime start = date.Date;
+        DateTime end = start.AddDays(1);
+
+        cmd.CommandText =
+            "SELECT FlightID, TailNumber, Destination, Origin, ArrivalTime, DepartureTime, " +
+            "LegroomFee, DefaultPrice, MealFee, ChosenSeatFee, ExtraLuggageFee " +
+            "FROM Flight " +
+            "WHERE Destination = @Destination " +
+            "AND Origin = @Origin " +
+            "AND DepartureTime >= @Start " +
+            "AND DepartureTime < @End;";
+
+        cmd.Parameters.AddWithValue("@Destination", destination);
+        cmd.Parameters.AddWithValue("@Origin", origin);
+        cmd.Parameters.AddWithValue("@Start", start.ToString("yyyy-MM-dd HH:mm:ss"));
+        cmd.Parameters.AddWithValue("@End", end.ToString("yyyy-MM-dd HH:mm:ss"));
+
+        using var reader = cmd.ExecuteReader();
+        while (reader.Read())
+        {
+            results.Add(new FlightModel());
+        }
+
+        return results;
+    }
+}
