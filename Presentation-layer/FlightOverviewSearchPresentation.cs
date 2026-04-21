f88e0c6b67fd680a4a0e0fc83847d9951b198e74
TiboSnoei <1117063@hr.nl>
2026-04-21 15:28:03 +0200

    flight presentation

--- Presentation-layer/FlightOverviewSearchPresentation.cs
+++ Presentation-layer/FlightOverviewSearchPresentation.cs
@@ -2,24 +2,66 @@ public class FlightOverviewSearchPresentation
 {
     public void FlightSearchMenu()
     {
+        string destinationselected;
+        DateTime departuredate;
+        bool returnflightselected;
+        DateTime returndate;
+
         // TODO: imploment search menu
-
-        // INFO: 4 lines below are dummy data
-        string destinationselected = "Berlin";
-        bool returnflightselected = true;
-        DateTime departuredate = DateTime.Parse("2026-05-01 00:00:00");
-        DateTime returndate = DateTime.Parse("2026-06-01 00:00:00");
-
-        if (!returnflightselected)
+        Console.Clear();
+        Console.WriteLine("===Search Flights===");
+        destinationselected = GetValidString("Destination: ");
+        departuredate = GetValidDate("Flight Date (yyyy-mm-dd): ");
+        string[] options = {"Yes", "No"};
+        Menu menu = new Menu();
+        string returnflightawnser = menu.VerticalMenu(options, "Do you want a return flight");
+        // string returnflightawnser = GetValidString("Do you want a return flight (yes/no)");
+        if (returnflightawnser == "Yes") 
         {
-            FlightOverviewCreator flightoverviewcreator = new FlightOverviewCreator(destinationselected, returnflightselected, departuredate);
-            flightoverviewcreator.GenerateFlightOverview();
+            returnflightselected = true;
+            returndate = GetValidDate("Return Flight Date (yyyy-mm-dd)");
+
+            FlightOverviewCreator flightOverviewCreator = new FlightOverviewCreator(destinationselected, returnflightselected, departuredate, returndate);
+            flightOverviewCreator.GenerateFlightOverview();
         }
 
-        else if (returnflightselected)
+        else if (returnflightawnser == "No") 
         {
-            FlightOverviewCreator flightoverviewcreator = new FlightOverviewCreator(destinationselected, returnflightselected, departuredate, returndate);
-            flightoverviewcreator.GenerateFlightOverview();
+            returnflightselected = false;
+
+            FlightOverviewCreator flightOverviewCreator = new FlightOverviewCreator(destinationselected, returnflightselected, departuredate);
+            flightOverviewCreator.GenerateFlightOverview();
+        }
+
+        else;
+    }
+
+    private string GetValidString(string message)
+    {
+        string input;
+        while (true)
+        {
+            Console.Write(message);
+            input = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(input))
+                return input;
+
+            Console.WriteLine("Cannot be empty.");
+        }
+    }
+
+    private DateTime GetValidDate(string message)
+    {
+        DateTime value;
+        while (true)
+        {
+            Console.Write(message);
+            string date = Console.ReadLine();
+            date = $"{date} 00:00:00";
+            if (DateTime.TryParse(date, out value))
+                return value;
+
+            Console.WriteLine("Invalid date.");
         }
     }
 }
