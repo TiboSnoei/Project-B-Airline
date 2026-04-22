ef34f3013f89b9b647fcb005ee2915847e829685
TiboSnoei <1117063@hr.nl>
2026-04-22 12:23:30 +0200

    Login and register during booking

--- Presentation-layer/BookFlightPresentation.cs
+++ Presentation-layer/BookFlightPresentation.cs
@@ -3,7 +3,6 @@ public class BookFlight
     public BookFlight(FlightModel chosenflight)
     {
         // TODO: Console.Clear in menuhandler moet verwijderd worden en moet hier lokaal gebeuren.
-        
         Console.WriteLine($"You have selected flight {chosenflight.FlightId} from {chosenflight.Origin} to {chosenflight.Destination}.");
         Console.WriteLine($"The price for this flight is {chosenflight.DefaultPrice}."); //TODO: price laten zien + prijs van de extras
         Console.WriteLine("Do you want to book this flight?");
@@ -15,8 +14,8 @@ public class BookFlight
         switch (choice)
         {
             case "Yes":
+                // TODO: Validation, laat een error message zien als het niet lukt en laat de console.writeline dan niet zien.
                 // TODO: assign seat (Zie BookFlightBusiness.cs)
-                Console.WriteLine("Booking confirmed! Log in to or create your account to check your booking details.");
 
                 var customerflight = new CustomerFlightModel
                 {
@@ -33,6 +32,27 @@ public class BookFlight
                 BookFlightAccess bookFlightAccess = new BookFlightAccess();
                 bookFlightAccess.Write(customerflight);
 
+                Console.Clear();
+                Console.WriteLine("You're almost done! Please log in or create an account to complete your booking.");
+
+                string[] options2 = { "Log In", "Create Account" };
+                Menu menu2 = new Menu();
+                string choice2 = menu2.VerticalMenu(options2, "Do you want to log in or register a new account?");
+
+                switch (choice2)
+                {
+                    case "Log In":
+                        AccountPresentation login = new AccountPresentation();
+                        login.Login();
+                        Console.ReadKey();
+                        break;
+
+                    case "Create Account":
+                        AccountPresentation register = new AccountPresentation();
+                        register.Register();
+                        Console.ReadKey();
+                        break;
+                }
                 break;
 
             case "No":
