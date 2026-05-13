2bca20f4c500d2b31ab090b1e2a71f0b7a53aeda
TiboSnoei <1117063@hr.nl>
2026-05-13 15:07:27 +0200

    fixed broken file sturcture due to merge

---
+++ Project-B-Airline.Tests/TestExcample.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+
+namespace Project_B_Airline.Tests;
+
+[TestClass]
+public class TestTest
+{
+    [TestMethod]
+    public void Test_ReturnNotImplemented()
+    {
+        // Add unit test logic
+        string result = "Not Implemented";
+        Assert.AreEqual("Not Implemented", result);
+    }
+}
